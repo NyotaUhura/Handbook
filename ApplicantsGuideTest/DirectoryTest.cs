38cc1f44c315eba4ecc6bd463100237713226d83
anastasiia.grynenko@gmail.com <anastasiia.grynenko@gmail.com>
2020-05-24 22:35:07 +0300

    add test method2

--- ApplicantsGuideTest/DirectoryTest.cs
+++ ApplicantsGuideTest/DirectoryTest.cs
@@ -14,11 +14,11 @@ namespace ApplicantsGuideTest
             directory.FillTestData(100);
             Assert.AreEqual("Spetiality0", directory.Universities[0].Spetialities[0].Name);
         }
-        //public void TestMethod2()
-        //{
-        //    Directory directory = new Directory();
-        //    directory.FillTestData(100);
-        //    Assert.AreEqual("Spetiality0", directory.Universities[0].Spetialities[0].Name);
-        //}
+        public void TestMethod2()
+        {
+            Directory directory = new Directory();
+            directory.FillTestData(100);
+            Assert.AreEqual("Spetiality0", directory.Universities[0].Spetialities[0].Name);
+        }
     }
 }
